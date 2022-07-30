84fb842ef04bcd106dad1b68bd58eb905b63e7ee
raz <raz@lupercio.mx>
2022-07-30 16:21:11 -0500

    Changed ExampleComponent to CoverComponente
    makes more sense ;)

---
+++ resources/js/frontend/components/CoverComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <span>Registro de Asistencias v0.0.1</span>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
